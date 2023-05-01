6664d635b1e5646b33ee1e1268e30a34d5808b25
https://github.com/lamnaimohamed/ <lamnaimohamed@gmail.com>
2023-05-01 14:38:05 +0100

    test 04

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -69,7 +69,7 @@ public class Controller {
 
     @GetMapping(value = "/helloword")
     public String hellow(){
-        return " bonjour tous le monde test 02 ";
+        return " bonjour tous le monde test 07 ";
     }
 
 }
